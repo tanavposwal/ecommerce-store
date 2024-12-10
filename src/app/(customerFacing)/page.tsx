6881201f49f904f8dff8cea7c76a0d22c2ef5a27
tanavposwal <tanavposwal939@gmail.com>
2024-12-10 09:02:00 +0530

    app is ready

--- src/app/(customerFacing)/page.tsx
+++ src/app/(customerFacing)/page.tsx
@@ -2,24 +2,20 @@ import { Button } from "@/components/ui/button";
 import db from "@/db/db";
 import { Product } from "@prisma/client";
 import { Suspense } from "react";
-import { ProductCard, ProductCardSkeleton } from "@/components/ProductCard"
+import { ProductCard, ProductCardSkeleton } from "@/components/ProductCard";
 import Link from "next/link";
 import { ArrowRightIcon } from "@heroicons/react/24/solid";
 
-function wait(duration: number) {
-    return new Promise(resolve => setTimeout(resolve, duration))
-}
-
-function getNewestProducts() {
-  return db.product.findMany({
+async function getNewestProducts() {
+  return await db.product.findMany({
     where: { isAvailableForPurchase: true },
     orderBy: { createdAt: "desc" },
     take: 6,
   });
 }
 
-function getMostPopularProducts() {
-  return db.product.findMany({
+async function getMostPopularProducts() {
+  return await db.product.findMany({
     where: { isAvailableForPurchase: true },
     orderBy: { orders: { _count: "desc" } },
     take: 6,
@@ -27,15 +23,14 @@ function getMostPopularProducts() {
 }
 
 export default function Home() {
-  return (
-    <main className="space-y-12">
-      <ProductGridSection
-        title="Most Popular"
-        productsFetcher={getMostPopularProducts}
-      />
-      <ProductGridSection title="Newest" productsFetcher={getNewestProducts} />
-    </main>
-  );
+  return;
+  <main className="space-y-12">
+    <ProductGridSection
+      title="Most Popular"
+      productsFetcher={getMostPopularProducts}
+    />
+    <ProductGridSection title="Newest" productsFetcher={getNewestProducts} />
+  </main>;
 }
 
 type productGridSectionProps = {
@@ -43,44 +38,37 @@ type productGridSectionProps = {
   title: string;
 };
 
-function ProductGridSection({
+async function ProductGridSection({
   productsFetcher,
   title,
 }: productGridSectionProps) {
-  return (
-    <div className="space-y-4">
-      <div className="flex gap-4">
-        <h2 className="text-3xl font-bold">{title}</h2>
-        <Button variant="outline" asChild>
-          <Link href="/products" className="space-x-2">
-            <span>View All</span>
-            <ArrowRightIcon className="w-4 h-4" />
-          </Link>
-        </Button>
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        <Suspense
-          fallback={
-            <>
-              <ProductCardSkeleton />
-              <ProductCardSkeleton />
-              <ProductCardSkeleton />
-            </>
-          }
-        >
-          <ProductSuspense productsFetcher={productsFetcher} />
-        </Suspense>
-      </div>
+  return;
+  <div className="space-y-4">
+    <div className="flex gap-4">
+      <h2 className="text-3xl font-bold">{title}</h2>
+      <Button variant="outline" asChild>
+        <Link href="/products" className="space-x-2">
+          <span>View All</span>
+          <ArrowRightIcon className="w-4 h-4" />
+        </Link>
+      </Button>
     </div>
-  );
-}
-
-async function ProductSuspense({
-  productsFetcher,
-}: {
-  productsFetcher: () => Promise<Product[]>;
-}) {
-  return (await productsFetcher()).map((product) => (
-    <ProductCard key={product.id} {...product} />
-  ));
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <Suspense
+        fallback={
+          <>
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+          </>
+        }
+      >
+        {(await productsFetcher()).map((product) => (
+          <>
+            <ProductCard key={product.id} {...product} />
+          </>
+        ))}
+      </Suspense>
+    </div>
+  </div>;
 }
